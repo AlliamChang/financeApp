70efa047523c6c16fba7f7915ebc235dd2f4bc32
AlliamChang <530685625@qq.com>
2017-09-05 20:59:32 +0800

    部分方法修改为post

--- app/profile_management/views.py
+++ app/profile_management/views.py
@@ -1,5 +1,8 @@
+# encoding:utf-8
+
 from . import app
 from flask import request
+# from app import db
 import json
 
 bank_cards = []
@@ -8,13 +11,18 @@ bank_cards = []
 # 得到绑定的银行卡信息
 @app.route('/getBankCard', methods=['GET'])
 def get_bank_card():
+    user_id = request.args.get('user_id')
+
     result = {"code": 0, "message": "", "data": bank_cards}
     return json.dumps(result)
 
 
 # 添加绑定的银行卡信息
-@app.route('/addBankCard', methods=['GET'])
+@app.route('/addBankCard', methods=['POST'])
 def add_bank_card():
+    user_id = request.args.get('user_id')
+    card_num = request.args.get('card_num')
+
     data = {"card_id": "", "bank_name": ""}
     card_id = request.args.get('bank_card')
     data["card_id"] = card_id
@@ -25,8 +33,11 @@ def add_bank_card():
 
 
 # 用户注册
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['POST'])
 def register():
+    user_id = request.args.get('user_id')
+    password = request.args.get('password')
+
     result = {"code": 0, "message": "success"}
     return json.dumps(result)
 
@@ -34,6 +45,9 @@ def register():
 # 用户登录
 @app.route('/login', methods=['GET'])
 def login():
+    user_id = request.args.get('user_id')
+    password = request.args.get('password')
+
     result = {"code": 0, "message": "success"}
     return json.dumps(result)
 
@@ -41,5 +55,7 @@ def login():
 # 获取用户信用报告 该报告在完成激活额度时会生成，在每次完成借贷后会更新
 @app.route('/getCreditReport', methods=['GET'])
 def get_credit_report():
+    user_id = request.args.get('user_id')
+
     result = {"code": 0, "message": "", "user_name": "Jack Ma", "score": 90, "detail": ""}
     return json.dumps(result)
--- app/standard_investment/views.py
+++ app/standard_investment/views.py
@@ -1,15 +1,21 @@
+# encoding:utf-8
+
 from . import app
 from flask import request
+# from app import db
 import json
 
 bid_list = []
 
 
 # 投资人参与标
-@app.route('/participateBid',methods=['GET'])
+@app.route('/participateBid',methods=['POST'])
 def participate_bid():
+    user_id = request.args.get('user_id')
+    bid_id = request.args.get('bid_id')
+    money = request.args.get('money')
+
     data = {"code": 0, "message": "success"}
-    bid_id = request.args.get('bid')
     bid_list.append(bid_id)
     return json.dumps(data)
 
@@ -17,6 +23,8 @@ def participate_bid():
 # 投资人获取所有的参与标
 @app.route('/getInvestedBids',methods=['GET'])
 def get_invested_bids():
+    user_id = request.args.get('user_id')
+
     data = {"code": 0, "message": "all", "bid_id":bid_list}
     return json.dumps(data)
 
@@ -24,7 +32,9 @@ def get_invested_bids():
 # 投资人获取参与的标的具体信息
 @app.route('/getInvestedBidDetail',methods=['GET'])
 def get_invested_bid_detail():
+    user_id = request.args.get('user_id')
     bid_id = request.args.get('bid')
+
     if bid_id in bid_list:
         data = {"code": 0, "message": "success", "bid_id": bid_id, "investmoney": 1000}
     else:
@@ -36,6 +46,7 @@ def get_invested_bid_detail():
 @app.route('/getInvestedLoans',methods=['GET'])
 def get_invested_loans():
     user_id = request.args.get('user_id')
+
     data = {"code": 0, "message": user_id, "loanIDs": [1,2,3]}
     return json.dumps(data)
 
@@ -43,13 +54,25 @@ def get_invested_loans():
 # 投资人获取指定贷款项的具体信息
 @app.route('/getInvestedLoanDetail',methods=['GET'])
 def get_invested_loan_detail():
+    user_id = request.args.get('user_id')
     loan_id = request.args.get('loan_id')
-    data = {"code": 0, "message": loan_id, "borrower_id": 123, "state": 0, "money": 1000, "repay_date": "2017-09-30", "repay_money": 500, "overdue_days": 0}
+
+    data = {"code": 0, "message": loan_id, "borrower_id": 123, "state": 0, "money": 1000, "repay_date": "2017-09-30",
+            "repay_money": 500, "overdue_days": 0}
     return json.dumps(data)
 
 
 # 获取所有的标的列表，供投资人浏览
 @app.route('/getAllBidsList',methods=['GET'])
 def get_all_bids_list():
+    money_start = request.args.get('money_range_start')
+    money_end = request.args.get('money_range_end')
+    sort_type = request.args.get('sort_type')
+
     data = {"code": 0, "message": "success", "bid_ids": [1,2,3,4,5,6,7,8,9]}
     return json.dumps(data)
+
+
+def error_msg(msg):
+    error = {"code": 0, "message": msg}
+    return json.dumps(error)
