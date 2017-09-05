9e2fe4eff83e5795af629a1b4c4bed7fb9c88b67
151250150 <151250150@smail.nju.edu.cn>
2017-09-05 20:18:54 +0800

    add:修改数据库账号密码

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+# encoding:utf-8
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,8 +10,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'M*qaEYfBjdOg*Ja#'
     # 按照你们数据库配置来修改此项 mysql://用户名:密码@服务器地址:端口号/数据库名称
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:wycg55967568w@localhost:3306/loan'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://p2p:p2p@120.27.199.164:3306/loan'
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db.init_app(app)
 
--- app/standard_investment/__init__.py
+++ app/standard_investment/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
 from flask import Blueprint
-from . import view
 
 app = Blueprint('standard_investment', __name__)
+from . import view
